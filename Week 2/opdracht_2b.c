1d2586dc5379f5f7d2a2dc66e0992f94399a6725
ahlau92 <andrew_henry_lau@hotmail.com>
2014-12-08 00:53:32 +0100

    Aanpassing
    Ik heb de code van toggle.c aangepast en een deel van opdracht 2b
    gemaakt.

--- Week 2/opdracht_2b.c
+++ Week 2/opdracht_2b.c
@@ -1,27 +1,12 @@
 #include <avr/io.h>
 #include <stdint.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
-void wait(void) {
-    TCNT1H	= 0x03;
-	TCNT1L	= 0x84;
-	/*TCNT1   = 900;*/
-	TIFR	= 1<<TOV1;
-	while (!(TIFR & 1<<TOV1)); 
-	/*while (TIFR != 0b00000100);*/
-}
-
-int main(void) {
-	void wait(void);
+ISR(TIMER1_OVF_vect) {
+    TCNT1 = 900;
+	
+	/* bepaal volgende patroon */  
 	uint8_t c1, c2, i, mode;
-
-	TCCR1A	&= ~(1<<WGM11 | 1<<WGM10);
-	TCCR1B	|=   1<<CS12  | 1<<CS10;
-	TCCR1B  &= ~(1<<WGM13 | 1<<WGM12 |1<<CS11);
-	/*TIMSK	 =   1<<TOIE1;*/
-
-	DDRA = 0x00;
-	DDRB = 0xFF;
 	mode = 0x00;
     while (1) {
 		if (PINA == 0x7F) {
@@ -35,7 +20,10 @@ int main(void) {
 			c1 = 0xC0;
 			for (i = 0; i < 4; i++) {
 				if (PINA != 0x7F) {
-					wait();
+				    /*vervangt wait()*/
+					TIFR	= 1<<TOV1;
+	   				while (!(TIFR & 1<<TOV1));
+					/* einde vervangt wait()*/
 					PORTB = ~(c1);
 					c1 >>= 2;
 				}
@@ -50,7 +38,10 @@ int main(void) {
 	        c2 = 0x00;
 	        for (i = 0; i < 4; i++) {
 				if (PINA != 0x7F) {
-		            wait();
+				    /*vervangt wait()*/
+					TIFR	= 1<<TOV1;
+	   				while (!(TIFR & 1<<TOV1));
+					/* einde vervangt wait()*/
 		            PORTB = ~(c1 | c2);
 		            c1 >>= 1;
 				}
@@ -60,7 +51,10 @@ int main(void) {
 	        }
 	        for (i = 0; i < 1; i++) {
 				if (PINA != 0x7F) {
-		            wait();
+				    /*vervangt wait()*/
+					TIFR	= 1<<TOV1;
+	   				while (!(TIFR & 1<<TOV1));
+					/* einde vervangt wait()*/
 		            PORTB = ~(c1 | c2);
 		            c1 <<= 1;
 				}
@@ -70,7 +64,10 @@ int main(void) {
 	        }
 	        for (i = 0; i < 5; i++) {
 				if (PINA != 0x7F) {
-		            wait();
+				    /*vervangt wait()*/
+					TIFR	= 1<<TOV1;
+	   				while (!(TIFR & 1<<TOV1));
+					/* einde vervangt wait()*/
 		            PORTB = ~(c1 | c2);
 		            c1 >>= 1;
 				}
@@ -79,7 +76,24 @@ int main(void) {
 				}
 	        }
 		}
-	}
-	return 0;
+	}  
+	/* einde bepaal volgende patroon */  
+
+
+
+    PORTB = ...;
 }
 
+int main(void) {
+	TCCR1A	&= ~(1<<WGM11 | 1<<WGM10);          /* normal mode */ 
+	TCCR1B  &= ~(1<<WGM13 | 1<<WGM12 |1<<CS11); /* normal mode & prescaler 2014*/ 
+	TCCR1B	|=   1<<CS12  | 1<<CS10;            /* prescaler 1024 */ 
+    TCNT1 = 900;
+    DDRB = 0xFF;
+    TIMSK |= 1<<TOEI1;
+    sei();
+    while (1) {
+        /* hier kun je iets anders doen */
+    }
+    return 0;
+}
