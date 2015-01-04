a471fcc51e99d8fe9a25b6110876f27b15dca12c
Thumbsy <kevin.alterative@gmail.com>
2015-01-04 19:36:07 +0100

    Initial assignment 5 files and some more 4a stuff

---
+++ Week 4a/noten_3686400.h
@@ -0,0 +1,55 @@
+#define	C0	14089
+#define	CIS0	13299
+#define	D0	12552
+#define	DIS0	11848
+#define	E0	11183
+#define	F0	10555
+#define	FIS0	9962
+#define	G0	9403
+#define	GIS0	8875
+#define	A0	8377
+#define	AIS0	7907
+#define	B0	7463
+
+#define	C1	7044
+#define	CIS1	6649
+#define	D1	6276
+#define	DIS1	5923
+#define	E1	5591
+#define	F1	5277
+#define	FIS1	4981
+#define	G1	4701
+#define	GIS1	4437
+#define	A1	4188
+#define	AIS1	3953
+#define	B1	3731
+
+#define	C2	3522
+#define	CIS2	3324
+#define	D2	3137
+#define	DIS2	2961
+#define	E2	2795
+#define	F2	2638
+#define	FIS2	2490
+#define	G2	2350
+#define	GIS2	2218
+#define	A2	2094
+#define	AIS2	1976
+#define	B2	1865
+
+#define	C3	1760
+#define	CIS3	1661
+#define	D3	1568
+#define	DIS3	1480
+#define	E3	1397
+#define	F3	1318
+#define	FIS3	1244
+#define	G3	1175
+#define	GIS3	1109
+#define	A3	1046
+#define	AIS3	987
+#define	B3	932
+
+#define	H	1	/*	herhaal	*/
+#define	R	2	/*	rust	*/
+#define	S	3	/*	stop	*/
--- Week 5/periodtimer.c
+++ Week 5/periodtimer.c
@@ -1,12 +1,14 @@
 #include <avr/io.h>
 #include <stdint.h>
 #include <avr/interrupt.h>
-#include "noten_3686400.h"
+#include "lcd.h"
 
+/*Not sure if ISR is needed*/
 ISR(insert isr here) {
 
 }
 
 int main(void) {
+	/*Insert code here*/
 	return 0;
 }
