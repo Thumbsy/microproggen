a92cc8d152c6236f531083c1a1a3734127e78956
Thumbsy <kevin.alterative@gmail.com>
2015-01-04 13:55:15 +0100

    Kicking off week 4 chiptune generator
    For my personal pleasure. Even if not used for the actual practicum, it
    can prove useful for the assignment that we do receive.

---
+++ Week 4a/chiptunes.c
@@ -0,0 +1,12 @@
+#include <avr/io.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include "noten_3686000.h"
+
+ISR(insert isr here) {
+
+}
+
+int main(void) {
+	return 0;
+}
---
+++ Week 4a/noten_3686000.h
@@ -0,0 +1,55 @@
+#define	C0	14088
+#define	CIS0	13297
+#define	D0	12551
+#define	DIS0	11846
+#define	E0	11181
+#define	F0	10554
+#define	FIS0	9961
+#define	G0	9402
+#define	GIS0	8874
+#define	A0	8376
+#define	AIS0	7906
+#define	B0	7462
+
+#define	C1	7043
+#define	CIS1	6648
+#define	D1	6275
+#define	DIS1	5923
+#define	E1	5590
+#define	F1	5276
+#define	FIS1	4980
+#define	G1	4701
+#define	GIS1	4437
+#define	A1	4188
+#define	AIS1	3953
+#define	B1	3731
+
+#define	C2	3521
+#define	CIS2	3324
+#define	D2	3137
+#define	DIS2	2961
+#define	E2	2795
+#define	F2	2638
+#define	FIS2	2490
+#define	G2	2350
+#define	GIS2	2218
+#define	A2	2093
+#define	AIS2	1976
+#define	B2	1865
+
+#define	C3	1760
+#define	CIS3	1661
+#define	D3	1568
+#define	DIS3	1480
+#define	E3	1397
+#define	F3	1318
+#define	FIS3	1244
+#define	G3	1174
+#define	GIS3	1108
+#define	A3	1046			
+#define	AIS3	987			
+#define	B3	932			
+
+#define	H	1	/*	herhaal	*/
+#define	R	2	/*	rust	*/
+#define	S	3	/*	stop	*/
