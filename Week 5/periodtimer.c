e74fb0b934f3e9d0720a1413e3c168ebbab068bf
Thumbsy <kevin.alterative@gmail.com>
2015-01-09 00:08:18 +0100

    Clean up current code a little
    Added my own idea of what the program should look like as well. In any
    case, the current program does not function I'm afraid.

--- Week 5/periodtimer.c
+++ Week 5/periodtimer.c
@@ -7,36 +7,34 @@
 int main(void) {
 	uint16_t period_clk, period_us ;
 
-	PORTD = 0xFF; 								/*activate pull-up*/
+	PORTD = 0xFF; 							/* Activate pull-up */
 
-	TCCR1A 	= 0;								/* Timer Mode = normal*/
-	TCCR1B 	= 0x42;								/* Capture on rising edge, noise canceller disabled, prescaler 8 */
-	ACSR   	= 0;
+	TCCR1A 	= 0;							/* Timer Mode = normal */
+	TCCR1B 	= 0x42;							/* Capture on rising edge, noise canceller disabled, prescaler 8 */
+	ACSR   	|= (1<<ACD);					/* Turn off Analog Comparator to save power */
 
-	lcd_init();									/* Initialise the LCD display */
-	lcd_cursor(false, false);					/* Turn off cursor */
+	lcd_init();								/* Initialise the LCD display */
+	lcd_cursor(false, false);				/* Turn off cursor */
 
-	TCNT1 	= 0;								/* Set TCNT1 zero */
+	TCNT1 	= 0;							/* Set TCNT1 zero */
 
-	while (1){
-		while ((TIFR& (1<<ICF1)) == 0);
-		TCNT1 		= 0;						/* Making sure that TCNT1 begins at 0 */
-		TIFR		= (1<<ICF1);				/* Clear ICF1 on first */
+	while ((TIFR & (1<<ICF1)) == 0);
+	TCNT1 		= 0;						/* Making sure that TCNT1 begins at 0 */
+	TIFR		= (1<<ICF1);				/* Clear ICF1 on first */
 
-		while ((TIFR& (1<<ICF1)) == 0);
-		period_clk 	= ICR1;
-		TIFR		= (1<<ICF1);				/* Clear ICF1 on second */
+	while ((TIFR & (1<<ICF1)) == 0);
+	period_clk 	= ICR1;
+	TIFR		= (1<<ICF1);				/* Clear ICF1 on second */
 
-		period_us	= period_clk * 2.17;	/* Niet zeker als je zo 2,17 kan gebruiken. Of alleen hele getallen? */	/* Calculating the actual period in microseconds */
+	period_us	= period_clk * 2.17;		/* Niet zeker als je zo 2,17 kan gebruiken. Of alleen hele getallen? */	/* Calculating the actual period in microseconds */
 
-		lcd_cls();
-		lcd_puts("De periodetijd");
-		lcd_goto(1, 0);
-		lcd_puts("is: %d us", period_us); /* Andrew is niet zeker als het zo gedaan moet worden. Is het een %d of een %10d? Of iets anders? */
-		
-		while(1){
-			/* Stay in here, FOREVER! */
-		}
+	lcd_cls();
+	lcd_puts("De periodetijd");
+	lcd_goto(1, 0);
+	lcd_puts("is: %d us", period_us);		/* Andrew is niet zeker als het zo gedaan moet worden. Is het een %d of een %10d? Of iets anders? */
+	
+	while(1){
+		/* Stay in here, FOREVER! */
 	}
 	return 0;
 }
